bb7c83c115c154c184cd747e62cc3c0e0645657c
movie42 <movie3342@gmail.com>
2023-04-26 21:26:55 +0900

    feat : Name Space

--- src/helloworld/helloworld.cpp
+++ src/helloworld/helloworld.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main(){
-    using namespace std;
+    using std::cout;
+    using std::endl;
     cout << "C++의 세계에 오신 것을 환영 합니다.";
     cout << endl;
     cout << "후회 nono" << endl;
