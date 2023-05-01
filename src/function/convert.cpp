13eb9cb1640e14991369c1eaf67be5bab2c96e27
movie42 <movie3342@gmail.com>
2023-05-01 17:56:31 +0900

    feat : function

---
+++ src/function/convert.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int stonetolb(int stone);
+
+int main() {
+    using namespace std;
+    int stone;
+    cout << "체중을 스톤 단위로 입력하시오 : ";
+    cin >> stone;
+    int pounds = stonetolb(stone);
+    cout << stone << " 스톤은 ";
+    cout << pounds << " 파운드입니다." << endl;
+    return 0;
+}
+
+int stonetolb(int sts) { return 14 * sts; }
