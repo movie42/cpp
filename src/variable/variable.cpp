8957d2c344b8a59a6f47fcf98d15f25487d6ad43
HYUNSU <movie3342@gmail.com>
2023-10-07 21:11:27 +0900

    feat : Variable

---
+++ src/variable/variable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+
+    double price = 10.99;
+    double gpa = 2.5;
+
+    // single character
+    char grade = 'A';
+
+    // boolean
+
+    bool student = true;
+    bool power = false;
+
+    // string
+    std::string name = "gg";
+    std::cout << name << std::endl;
+
+    // const
+
+    const double PI = 3.14159;
+    // PI = 1; 바꿀 수  없다.
+
+    return 0;
+}
